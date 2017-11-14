b5bac0872e2e9c44bce0fd1c861af6fdaa13bd3f
playra <raoffonom@icloud.com>
2017-11-14 22:59:16 +0300

    relations

--- src/model/tweet.js
+++ src/model/tweet.js
@@ -1,9 +1,25 @@
 import mongoose from 'mongoose'
+import { UserTC } from './user'
 import composeWithMongoose from 'graphql-compose-mongoose'
 
 const TweetSchema = new mongoose.Schema({
-  text: String
+  text: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 }, { timestamps: true } )
 
 export const Tweet = mongoose.model('Tweet', TweetSchema)
 export const TweetTC = composeWithMongoose(Tweet)
+
+TweetTC.addRelation(
+  'user',
+  {
+    resolver: () => UserTC.getResolver('findById'),
+    prepareArgs: {
+      _id: source => source.userId
+    },
+    projection: { userId: true },
+  }
+)
--- src/model/user.js
+++ src/model/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { TweetTC } from './tweet'
 import composeWithMongoose from 'graphql-compose-mongoose'
 
 const UserSchema = new mongoose.Schema({
@@ -10,8 +11,24 @@ const UserSchema = new mongoose.Schema({
   lastName: String,
   avatar: String,
   password: String,
-  email: String
+  email: String,
+  tweetsIds: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tweet'
+  }],
 }, { timestamps: true })
+
 
 export const User = mongoose.model('User', UserSchema)
 export const UserTC = composeWithMongoose(User)
+
+UserTC.addRelation(
+  'tweets',
+  {
+    resolver: () => TweetTC.getResolver('findByIds'),
+    prepareArgs: {
+      _ids: (source) => source.tweetsIds || [],
+    },
+    projection: { tweetsIds: true },
+  }
+)
